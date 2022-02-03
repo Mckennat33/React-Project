25f7552b09e65af48ce5ac41c2c10461b1b56aad
Thomas <thomasmckenna12gmail.com>
2022-02-02 23:21:26 -0500

    added notes

--- src/App.js
+++ src/App.js
@@ -6,36 +6,11 @@ import './App.css';
 import weather from "./weather.json"
 
 function App() {
-//   const weatherAPI = "http://api.weatherstack.com/current?access_key=ea480034a53a0fca6f27d7a599482ae8&query=`${location}`"
-
-// const goodWeatherAPI = "https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly?lat=35.5&lon=-78.5"
-const [ searchedCity, setSearchedCity ] = useState("Detroit")
-
-useEffect(() => {
-  fetch(`https://weatherapi-com.p.rapidapi.com/forecast.json?q=${searchedCity}&days=3`, {
-    "method": "GET",
-    "headers": {
-      "x-rapidapi-host": "weatherapi-com.p.rapidapi.com",
-      "x-rapidapi-key": "de51889a1fmshe095099b1a97993p13134fjsnc818ad7373cb"
-    }
-  })
-  .then(res => res.json())
-  .then(data => setSearchedCity(data))
-  .catch(err => {
-    console.error(err);
-  });
-}, [])
-
-
-
-  function handleSearchChange(city) {
-    console.log(city)
-}
 
 return (
     <div className="App">
       <h1>Weather App</h1>
-      <SearchBar onSearchChange={handleSearchChange}  />
+      <SearchBar />
     </div>
   );
 }
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,23 +1,58 @@
 import React from "react"
 import WeatherDisplay from "./WeatherDisplay";
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { scryRenderedComponentsWithType } from "react-dom/test-utils";
 
-function SearchBar({ onSearchChange }) {
+function SearchBar() {
+
+// first goal
+// Get the api/weather information on onto the page. 
+// we want the weather to display for a random city. Then we want the weather to display for the city that is searched.
+// What I am thinking is that we useEffect then so it fetched the data. Then when we search a new city it updated that fetch request with 
+// the city that is subbed 
+
+// look up useEffect and its second arguments. 
+
+
+    const [ searchedCity, setSearchedCity ] = useState("Detroit")
+    const [ weather, setWeather ] = useState('')
+
+    useEffect(() => {
+        fetch(`https://weatherapi-com.p.rapidapi.com/forecast.json?q=${searchedCity}&days=3`, {
+            "method": "GET",
+            "headers": {
+            "x-rapidapi-host": "weatherapi-com.p.rapidapi.com",
+            "x-rapidapi-key": "de51889a1fmshe095099b1a97993p13134fjsnc818ad7373cb"
+        }
+        })
+        .then(res => res.json())
+        .then(data => setSearchedCity(data))
+        .catch(err => {
+        console.error(err);
+    });
+    }, [])
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        console.log(event)
+    }
+
 
     return (
         <div>
             <form 
-            className="search-form">
+            className="search-form"
+            onSubmit={handleSubmit}
+            >
             <label>Search</label>
                 <input 
                 text="text" 
                 id="search"
-                onChange={e => onSearchChange(e.target.value)}
+                onChange={e => setSearchedCity(e.target.value)}
                 />
+                <button>Submit</button>
             <WeatherDisplay />
             </form>
-
         </div>
     )
 }
